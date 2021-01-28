c0dc9938057e873cf6df0375434b54261b9c0364
Yesh Doctor <yesh.doctor@duke.edu>
2021-01-28 16:13:20 -0500

    Added HDL Checker

--- blood_tests.py
+++ blood_tests.py
@@ -6,4 +6,13 @@ def interface():
 		choice = input("Enter an option:")
 		if choice == "9":
 			return
+
+def check_HDL(val):
+	if val > 60 : return "Normal"
+	elif val > 40 : return "Borlerline Low"
+	else: return "Low"
+
+
+
+
 interface()
