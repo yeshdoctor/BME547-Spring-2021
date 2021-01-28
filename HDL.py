ad1bd59a3def350f75ff1931cddd095b0dbaeee9
Yesh Doctor <yesh.doctor@duke.edu>
2021-01-28 16:38:24 -0500

    Completed LDL Driver

--- HDL.py
+++ HDL.py
@@ -1,9 +1,11 @@
 def interface():
     while(True):
         print("Blood Test Analysis")
-        response = input('Choose an option (1-HDL, 9-Quit)')
+        response = input('Choose an option (1-HDL, 2-LDL, 9-Quit)')
         if response == "1":
             HDLdriver()
+        if response == "2":
+            LDLdriver()
         elif response ==  "9":
             return
 def HDLdriver():
@@ -11,6 +13,12 @@ def HDLdriver():
     HDL = int(response)
     analysis = analyze_HDl(HDL)
     output_HDL(HDL,analysis)
+
+def LDLdriver():
+        response = input_LDL()
+        LDL = int(response)
+        analysis = analyze_LDL(LDL)
+        output_LDL(LDL,analysis)
 
 def analyze_HDl(HDL):
     if HDL >= 60: return "Normal"
@@ -22,5 +30,16 @@ def output_HDL(HDL,analysis):
 
 def input_HDL():
     return(input("Input HDL level:"))
+
+def analyze_LDL(LDL):
+    if LDL <= 130: return "Normal"
+    elif 158>LDL>=130: return "Borderline High"
+    else: return "High"
+
+def output_LDL(LDL,analysis):
+    print("The LDL level of {L} is {A}".format(L=LDL,A=analysis))
+
+def input_LDL():
+    return(input("Input LDL level:"))
 
 interface()
